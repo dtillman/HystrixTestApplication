2917376dea4b53e66c206e1a487eca304fc9f086
Dave Tillman <dtillman@pivotal.io>
2018-01-22 16:24:07 -0700

    Initial commit

---
+++ WebApplication8/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TestCommand _command;
+        public HomeController(TestCommand command)
+        {
+            _command = command;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = _command.Execute();
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication8/TestCommand.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Steeltoe.CircuitBreaker.Hystrix;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8
+{
+    public class TestCommand : HystrixCommand<bool>
+    {
+        private ILogger<TestCommand> _logger;
+
+        public TestCommand(IHystrixCommandOptions options,
+           ILogger<TestCommand> logger) : base(options)
+        {
+            _logger = logger;
+            IsFallbackUserDefined = true;
+        }
+        protected override bool Run()
+        {
+            // throw new Exception("Bang!");
+            return true;
+        }
+
+        protected override bool RunFallback()
+        {
+            return false;
+        }
+    }
+}
